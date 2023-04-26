692cef77a12ee16350da90835970096cf6f434fe
q12p <q.pw.q12@gmail.com>
2023-04-26 15:35:38 +0200

    .

---
+++ chatgpt.py
@@ -0,0 +1,22 @@
+import openai
+
+
+openai.api_key= 'WRITE API KEY HERE'
+
+messages = [
+    {"role": "system", "content": "You are a kind helpful assistant."},
+]
+
+while True:
+    message = input("\n\n\n\n\033[0m User : ")
+    if message:
+        messages.append(
+            {"role": "user", "content": message},
+        )
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages
+        )
+    
+    reply = chat.choices[0].message.content
+    print(f"\n\n\n\n\033[96m ChatGPT: {reply}")
+    messages.append({"role": "assistant", "content": reply})
